bdcf2bd764b5b5614665a480b0df24f03dc73e94
Kyle Adams <kyle@codingkyle.com>
2014-03-18 10:53:52 +0100

    Initial Commit

---
+++ TopPlaces/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TopPlaces
+//
+//  Created by Kyle Adams on 18-03-14.
+//  Copyright (c) 2014 Kyle Adams. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ TopPlaces/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TopPlaces
+//
+//  Created by Kyle Adams on 18-03-14.
+//  Copyright (c) 2014 Kyle Adams. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
